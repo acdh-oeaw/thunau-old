c4ad0d18093b0fe118d9551734b6aa85dee37c2c
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2017-06-26 08:55:16 +0200

    updated link to github repo

--- webpage/metadata.py
+++ webpage/metadata.py
@@ -5,8 +5,8 @@ PROJECT_METADATA = {
     'title': 'Thunau',
     'author': 'Peter Andorfer',
     'subtitle': 'Thunau-Excavation Data Manager',
-    'description': 'A web application to manage and publish data gathered around the excavations in Thunau',
-    'github': 'https://github.com/acdh-oeaw/',
+    'description': 'A web application to manage and publish data gathered at excavations in Thunau',
+    'github': 'https://github.com/acdh-oeaw/thunau',
     'purpose_de': 'Ziel des Projektes ist die Publikation von Forschungsdaten.',
     'purpose_en': 'The purpose of this Project is the publication of research data',
     'version': '1.0.0'
